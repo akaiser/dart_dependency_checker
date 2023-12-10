8095bc5ea8bfac9cae5675a8d5893b29924ee86d
Albert Kaiser <ak@kaiserv.de>
2023-12-10 02:58:21 +0100

    cleanup

--- bin/dart_dependency_checker.dart
+++ bin/dart_dependency_checker.dart
@@ -1,5 +1,5 @@
-import 'dart:io';
+import 'dart:io' show exit;
 
-import 'package:dart_dependency_checker/dart_dependency_checker.dart' as ddc;
+import 'package:dart_dependency_checker/dart_dependency_checker.dart' show run;
 
-void main(List<String> args) => exit(ddc.run(args).code);
+void main(List<String> args) => exit(run(args).code);
