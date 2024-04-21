02fcbe8115e0591da75fe9b9a34bd74b0b3fecfb
Albert Kaiser <ak@kaiserv.de>
2024-04-21 23:15:39 +0200

    expose BaseResults in the lib

--- lib/src/_exports.dart
+++ lib/src/_exports.dart
@@ -1,2 +1,3 @@
+export 'package:dart_dependency_checker/src/_shared/results.dart';
 export 'package:dart_dependency_checker/src/checker.dart';
 export 'package:dart_dependency_checker/src/checker_error.dart';
