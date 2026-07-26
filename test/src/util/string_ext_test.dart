b4bb798cb4790ec8d31bcd68b1c143441d6a45e6
Albert Kaiser <ak@kaiserv.de>
2026-07-26 20:43:33 +0200

    more tests

--- test/src/util/string_ext_test.dart
+++ test/src/util/string_ext_test.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:dart_dependency_checker/src/util/file_ext.dart';
 import 'package:dart_dependency_checker/src/util/string_ext.dart';
 import 'package:test/test.dart';
 
@@ -7,12 +8,31 @@ import '../_paths.dart';
 
 void main() {
   group('file', () {
-    test('reads existing file', () {
+    test('existing', () {
       expect('$exportImportPath/import.dart'.file.existsSync(), isTrue);
     });
 
-    test('reads non existing file', () {
+    test('non existing', () {
       expect('any.dart'.file.existsSync(), isFalse);
+    });
+  });
+
+  group('read', () {
+    test('existing', () {
+      expect('$exportImportPath/import.dart'.file.read, isNotEmpty);
+    });
+
+    test('non existing', () {
+      expect(
+        () => 'any.dart'.file.read,
+        throwsA(
+          isA<PathNotFoundException>().having(
+            (e) => e.message,
+            'message',
+            startsWith('Cannot open file'),
+          ),
+        ),
+      );
     });
   });
 
